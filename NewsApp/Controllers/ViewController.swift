3360694af73c79fd23e232325bf08cfb6da39d08
Shagara Nasution <shagara@ruangguru.com>
2020-06-07 19:36:56 +0700

    First Init

---
+++ NewsApp/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Shagara F Nasution on 07/06/20.
+//  Copyright © 2020 Shagara F Nasution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
